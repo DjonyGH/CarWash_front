25d19211e290d93350df2294e9f527ca0152eb03
Evgeniy Kalimullin <counsil1986@gmail.com>
2021-06-15 01:04:25 +0700

    Работа над главной страницей

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { Social } from './Social'
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <div></div>
+      <div className={styles.phone}> 8-951-184-3737</div>
       <Social />
     </footer>
   )
